799722ceba7a125cfb1db1fb5756046990ee2375
kenhardika <kenzamahardika@gmail.com>
2022-12-10 06:45:33 +0700

    update fix bugs in ClassIndex, now functions works

--- pages/ClassIndex.js
+++ pages/ClassIndex.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 import InputSectionClass from '../components_class/inputSectionClass';
@@ -8,7 +8,7 @@ import ListClass from '../components_class/InputSectionComponents/ListClass';
 import ModalCropperClass from '../components_class/ModalCropperClass';
 import ModalLayerClass from '../components_class/ModalLayerClass';
 
-class ClassIndex extends PureComponent {
+class ClassIndex extends Component {
   constructor(props) {
     super(props);
     this.state = {
